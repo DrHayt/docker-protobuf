9796cf5d6606493905f4094cf70ce8fa15c0766e
Rick Moran <drhayt@gmail.com>
2018-10-30 11:23:29 -0400

    Made go only branch

---
+++ buildall.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -x
+export GOPATH=/go
+
+find $GOPATH/src/golang.sgpdev.com/conduit/models -type f -name \*.pb.go | xargs rm
+
+find sgt 	-type f -name \*.proto | xargs -n 1 protoc --go_out=$GOPATH/src
+find services	-type f -name \*.proto | xargs -n 1 protoc --go_out=plugins=grpc:$GOPATH/src
